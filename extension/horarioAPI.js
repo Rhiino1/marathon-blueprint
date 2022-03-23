b21daf6cab3bfb045b7832fbd5e07e36749563cf
Cesar Solano <csolanoc@unal.edu.co>
2022-03-23 15:53:30 -0500

    Change column names fixed

--- extension/horarioAPI.js
+++ extension/horarioAPI.js
@@ -286,16 +286,16 @@ async function generalScheduleFunction(horarioData, count = 0) {
     if (horarioData.data.columns[index] === 'Juego') {
       juegoC = index;
     }
-    if (horarioData.data.columns[index] === 'Runners') {
+    if (horarioData.data.columns[index] === 'Runner/s') {
       runnerC = index;
     }
     if (horarioData.data.columns[index] === 'Categoría') {
       categoriaC = index;
     }
-    if (horarioData.data.columns[index] === 'Consola') {
+    if (horarioData.data.columns[index] === 'Plataforma') {
       plataformaC = index;
     }
-    if (horarioData.data.columns[index] === 'Comentarista') {
+    if (horarioData.data.columns[index] === 'Comentaristas') {
       comsC = index;
     }
     if (horarioData.data.columns[index] === 'hiddenYear') {
