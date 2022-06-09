cf5da6cbe3ac6fcc44cedccc29d6afbd0f008759
Cesar Solano <csolanoc@unal.edu.co>
2022-06-09 17:46:11 -0500

    169 complemento

---
+++ src/graphics/scripts/layout16-9-complemento.js
@@ -0,0 +1,25 @@
+import '../styles/layout16-9-complemento.css';
+
+const runner = document.querySelectorAll('.runner');
+const game = document.querySelectorAll('.game');
+const category = document.querySelectorAll('.category');
+const platform = document.querySelectorAll('.platform');
+const estimate = document.querySelectorAll('.estimate');
+const coms = document.querySelectorAll('.coms');
+
+const generalRunInfo = nodecg.Replicant('generalRunInfo');
+const players = nodecg.Replicant('players');
+
+NodeCG.waitForReplicants(generalRunInfo, players).then(() => {
+  players.on('change', (value) => {
+    runner[0].textContent = `${value.twitch[0]}`;
+  })
+  generalRunInfo.on('change', (value) => {
+    console.log(value);
+    game[0].textContent = value.game;
+    category[0].textContent = `${value.category}`;
+    platform[0].textContent = `${value.platform}`;
+    estimate[0].textContent = `${value.estimate}`;
+    coms[0].textContent = `${value.coms}`;
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,98 +2,104 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const dashboardConfig = {
-	entry: {
-		generalPanel: './src/dashboard/scripts/generalPanel.js',
-	},
-	output: {
-		filename: '[name].js',
-		path: path.join(__dirname, 'dashboard'),
-		assetModuleFilename: 'images/[hash][ext]'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'generalPanel.html',
-			template: './src/dashboard/generalPanel.html',
-			chunks: ['generalPanel']
-		}),
-	],
-	module: {
-		rules: [{
-			test: /\.css$/,
-			use: [
-				'style-loader',
-				'css-loader'
-			]
-		}, {
-			test: /\.(png|jpg|gif|ttf|mp4)$/i,
-			type: 'asset/resource'
-		}, ]
-	},
-	mode: 'production'
+  entry: {
+    generalPanel: './src/dashboard/scripts/generalPanel.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dashboard'),
+    assetModuleFilename: 'images/[hash][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'generalPanel.html',
+      template: './src/dashboard/generalPanel.html',
+      chunks: ['generalPanel']
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.(png|jpg|gif|ttf|mp4)$/i,
+      type: 'asset/resource'
+    }, ]
+  },
+  mode: 'production'
 };
 
 const graphicsConfig = {
-	entry: {
-		layoutEspera: ['./src/graphics/scripts/layoutEspera.js'],
-		layout169: ['./src/graphics/scripts/layout16-9.js'],
-		layout43: ['./src/graphics/scripts/layout4-3.js'],
-		layoutDs: ['./src/graphics/scripts/layoutDs.js'],
-		layoutCarreras2: ['./src/graphics/scripts/layoutCarreras2.js'],
-		layoutCarreras4: ['./src/graphics/scripts/layoutCarreras4.js'],
-	},
-	output: {
-		filename: '[name].js',
-		path: path.join(__dirname, 'graphics'),
-		assetModuleFilename: 'images/[hash][ext]'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'layoutEspera.html',
-			template: './src/graphics/layoutEspera.html',
-			chunks: ['layoutEspera']
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'layout16-9.html',
-			template: './src/graphics/layout16-9.html',
-			chunks: ['layout169']
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'layout4-3.html',
-			template: './src/graphics/layout4-3.html',
-			chunks: ['layout43']
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'layoutDs.html',
-			template: './src/graphics/layoutDs.html',
-			chunks: ['layoutDs']
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'layoutCarreras2.html',
-			template: './src/graphics/layoutCarreras2.html',
-			chunks: ['layoutCarreras2']
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'layoutCarreras4.html',
-			template: './src/graphics/layoutCarreras4.html',
-			chunks: ['layoutCarreras4']
-		}),
-	],
-	module: {
-		rules: [{
-			test: /\.css$/,
-			use: [
-				'style-loader',
-				'css-loader'
-			]
-		}, {
-			test: /\.(png|jpg|gif|ttf|mp4)$/i,
-			type: 'asset/resource'
-		}, ]
-	},
-	mode: 'production'
+  entry: {
+    layoutEspera: ['./src/graphics/scripts/layoutEspera.js'],
+    layout169: ['./src/graphics/scripts/layout16-9.js'],
+    layout169Complemento: ['./src/graphics/scripts/layout16-9-complemento.js'],
+    layout43: ['./src/graphics/scripts/layout4-3.js'],
+    layoutDs: ['./src/graphics/scripts/layoutDs.js'],
+    layoutCarreras2: ['./src/graphics/scripts/layoutCarreras2.js'],
+    layoutCarreras4: ['./src/graphics/scripts/layoutCarreras4.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'graphics'),
+    assetModuleFilename: 'images/[hash][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'layoutEspera.html',
+      template: './src/graphics/layoutEspera.html',
+      chunks: ['layoutEspera']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'layout16-9.html',
+      template: './src/graphics/layout16-9.html',
+      chunks: ['layout169']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'layout16-9-Complemento.html',
+      template: './src/graphics/layout16-9-complemento.html',
+      chunks: ['layout169Complemento']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'layout4-3.html',
+      template: './src/graphics/layout4-3.html',
+      chunks: ['layout43']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'layoutDs.html',
+      template: './src/graphics/layoutDs.html',
+      chunks: ['layoutDs']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'layoutCarreras2.html',
+      template: './src/graphics/layoutCarreras2.html',
+      chunks: ['layoutCarreras2']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'layoutCarreras4.html',
+      template: './src/graphics/layoutCarreras4.html',
+      chunks: ['layoutCarreras4']
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.(png|jpg|gif|ttf|mp4)$/i,
+      type: 'asset/resource'
+    }, ]
+  },
+  mode: 'production'
 };
 
 module.exports = [
-	dashboardConfig,
-	graphicsConfig
+  dashboardConfig,
+  graphicsConfig
 ];
